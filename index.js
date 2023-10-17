4902cc7e6af34f0af34a08f7ce81a6f1dad04b4f
eshr4q <feshr4q@gmail.com>
2023-10-17 19:54:20 +0330

    3

---
+++ index.js
@@ -0,0 +1,8 @@
+for (i=0; i<document.querySelectorAll(".drum").length; i++) {
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        alert("I got clicked!");
+
+    });
+
+}
