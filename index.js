fe3c0474d7295135777c043c337db22e9f29564e
eshr4q <feshr4q@gmail.com>
2023-10-19 13:54:58 +0330

    sound 6

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 for (i=0; i<document.querySelectorAll(".drum").length; i++) {
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-        alert("I got clicked!");
+
+        var audio = new Audio("sounds/tom-2.mp3");
+        audio.play();
+      
 
     });
 
